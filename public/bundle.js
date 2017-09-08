5fa16cfab90a53f8120c69863039d2664ce98676
Calvin McElroy <fourwallsstudio@gmail.com>
2017-09-08 18:21:23 -0400

    bundle

--- public/bundle.js
+++ public/bundle.js
@@ -69,7 +69,7 @@
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__three_three__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__three_three__ = __webpack_require__(1);
 
 
 
@@ -84,75 +84,8 @@ document.addEventListener('DOMContentLoaded', () => {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-const sequencerGrid = [];
-/* harmony export (immutable) */ __webpack_exports__["b"] = sequencerGrid;
-
-for (let i = 0; i < 16; i++) {
-  sequencerGrid.push([false, false, false, false])
-}
-
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-const analyser = audioCtx.createAnalyser();
-/* harmony export (immutable) */ __webpack_exports__["a"] = analyser;
-
-
-analyser.fftSize = 256;
-const bufferLength = analyser.frequencyBinCount;
-analyser.connect(audioCtx.destination);
-
-
-const sampleURLs = [
-  'assets/samples/BT0A0D0.WAV',
-  'assets/samples/ST0T0S7.WAV',
-  'assets/samples/HHCD0.WAV',
-  'assets/samples/HANDCLP1.WAV'
-]
-
-const getSample = (idx) => {
-  const source = audioCtx.createBufferSource();
-  const request = new XMLHttpRequest();
-
-    request.open('GET', sampleURLs[idx], true);
-
-    request.responseType = 'arraybuffer';
-
-    request.onload = function() {
-      const audioData = request.response;
-
-      audioCtx.decodeAudioData(audioData, function(buffer) {
-          source.buffer = buffer;
-          source.connect(analyser);
-          source.start(0);
-        },
-
-        function(e){ console.log("Error with decoding audio data" + e.err); });
-    }
-
-    request.send();
-}
-
-let currentCol = 0;
-
-const getGrid = () => sequencerGrid;
-
-setInterval( () => {
-  const row = getGrid()[currentCol];
-  if (row[0] === true) getSample(0);
-  if (row[1] === true) getSample(1);
-  if (row[2] === true) getSample(2);
-  if (row[3] === true) getSample(3);
-  currentCol = (currentCol + 1) % 16;
-}, 125);
-
-
-/***/ }),
-/* 2 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__sample__ = __webpack_require__(1);
-const THREE = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__sample__ = __webpack_require__(3);
+const THREE = __webpack_require__(2);
 
 
 
@@ -279,7 +212,7 @@ const animate = () => {
 
 
 /***/ }),
-/* 3 */
+/* 2 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -44514,6 +44447,73 @@ function CanvasRenderer() {
 }
 
 
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const sequencerGrid = [];
+/* harmony export (immutable) */ __webpack_exports__["b"] = sequencerGrid;
+
+for (let i = 0; i < 16; i++) {
+  sequencerGrid.push([false, false, false, false])
+}
+
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+const analyser = audioCtx.createAnalyser();
+/* harmony export (immutable) */ __webpack_exports__["a"] = analyser;
+
+
+analyser.fftSize = 256;
+const bufferLength = analyser.frequencyBinCount;
+analyser.connect(audioCtx.destination);
+
+
+const sampleURLs = [
+  'assets/samples/BT0A0D0.WAV',
+  'assets/samples/ST0T0S7.WAV',
+  'assets/samples/HHCD0.WAV',
+  'assets/samples/HANDCLP1.WAV'
+]
+
+const getSample = (idx) => {
+  const source = audioCtx.createBufferSource();
+  const request = new XMLHttpRequest();
+
+    request.open('GET', sampleURLs[idx], true);
+
+    request.responseType = 'arraybuffer';
+
+    request.onload = function() {
+      const audioData = request.response;
+
+      audioCtx.decodeAudioData(audioData, function(buffer) {
+          source.buffer = buffer;
+          source.connect(analyser);
+          source.start(0);
+        },
+
+        function(e){ console.log("Error with decoding audio data" + e.err); });
+    }
+
+    request.send();
+}
+
+let currentCol = 0;
+
+const getGrid = () => sequencerGrid;
+
+setInterval( () => {
+  const row = getGrid()[currentCol];
+  if (row[0] === true) getSample(0);
+  if (row[1] === true) getSample(1);
+  if (row[2] === true) getSample(2);
+  if (row[3] === true) getSample(3);
+  currentCol = (currentCol + 1) % 16;
+}, 125);
 
 
 /***/ })
