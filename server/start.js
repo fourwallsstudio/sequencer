0bf1ee941fe8a7d583d84d1afbc10b3ea55c6b2b
Calvin McElroy <fourwallsstudio@gmail.com>
2017-09-08 18:33:11 -0400

    add port var

--- server/start.js
+++ server/start.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log('app up');
 })
